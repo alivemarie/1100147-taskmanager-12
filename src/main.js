8cfaaaf5034fd67180fae15417586a03732fffcf
alivemarie <alivemarie@gmail.com>
2020-08-07 12:51:15 +0300

    Добавлены данные о дате карточки в шаблон

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import {createSiteMenuTemplate} from "./view/site-menu.js";
 import {createFilterTemplate} from "./view/filter.js";
 import {createBoardTemplate} from "./view/board.js";
 import {createSortingTemplate} from "./view/sort.js";
-import {createTaskAddTemplate} from "./view/task-add.js";
+import {createTaskEditTemplate} from "./view/task-edit.js";
 import {createTaskTemplate} from "./view/task.js";
 import {createButtonLoadMoreTemplate} from "./view/load-more-button";
 import {generateTask} from "./mock/task";
 
-const TASK_COUNT = 3;
+const TASK_COUNT = 4;
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -25,9 +25,9 @@ const taskListElement = boardElement.querySelector(`.board__tasks`);
 
 render(boardElement, createSortingTemplate(), `afterbegin`);
 
-render(taskListElement, createTaskAddTemplate());
+render(taskListElement, createTaskEditTemplate(tasks[0]));
 
-for (let i = 0; i < TASK_COUNT; i++) {
+for (let i = 1; i < TASK_COUNT; i++) {
   render(taskListElement, createTaskTemplate(tasks[i]));
 }
 
