f5c3c73cb77029f1fc84dcb8de15c6188a124bcf
alivemarie <alivemarie@gmail.com>
2020-08-08 09:51:11 +0300

    Добавлены данные цвета и повторения задачи, счетчик фильтров, и функционал допоказа карточек

--- src/main.js
+++ src/main.js
@@ -6,9 +6,13 @@ import {createTaskEditTemplate} from "./view/task-edit.js";
 import {createTaskTemplate} from "./view/task.js";
 import {createButtonLoadMoreTemplate} from "./view/load-more-button";
 import {generateTask} from "./mock/task";
+import {generateFilter} from "./mock/filter.js";
 
-const TASK_COUNT = 4;
+const TASK_COUNT = 22;
+const TASK_COUNT_PER_STEP = 8;
+
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
+const filters = generateFilter(tasks);
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
@@ -17,7 +21,7 @@ const siteMainElement = document.querySelector(`main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 render(siteHeaderElement, createSiteMenuTemplate());
-render(siteMainElement, createFilterTemplate());
+render(siteMainElement, createFilterTemplate(filters));
 render(siteMainElement, createBoardTemplate());
 
 const boardElement = siteMainElement.querySelector(`.board`);
@@ -27,8 +31,27 @@ render(boardElement, createSortingTemplate(), `afterbegin`);
 
 render(taskListElement, createTaskEditTemplate(tasks[0]));
 
-for (let i = 1; i < TASK_COUNT; i++) {
+for (let i = 1; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
   render(taskListElement, createTaskTemplate(tasks[i]));
 }
 
-render(boardElement, createButtonLoadMoreTemplate());
+if (tasks.length > TASK_COUNT_PER_STEP) {
+  let renderedTaskCount = TASK_COUNT_PER_STEP;
+  render(boardElement, createButtonLoadMoreTemplate());
+
+  const loadMoreButton = boardElement.querySelector(`.load-more`);
+
+  loadMoreButton.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    tasks
+      .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
+      .forEach((task) => render(taskListElement, createTaskTemplate(task)));
+
+    renderedTaskCount += TASK_COUNT_PER_STEP;
+
+    if (renderedTaskCount >= tasks.length) {
+      loadMoreButton.remove();
+    }
+  });
+}
+
--- src/mock/task.js
+++ src/mock/task.js
@@ -1,9 +1,5 @@
-const getRandomInteger = (a = 0, b = 1) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-
-  return Math.round(lower + Math.random() * (upper - lower));
-};
+import {COLORS} from "../const.js";
+import {getRandomInteger} from "../util";
 
 const generateDescription = () => {
   const descriptions = [
@@ -17,9 +13,8 @@ const generateDescription = () => {
 };
 
 const getRandomColor = () => {
-  const colors = [`black`, `yellow`, `blue`, `green`, `pink`];
-  const randomIndex = getRandomInteger(0, colors.length - 1);
-  return colors[randomIndex];
+  const randomIndex = getRandomInteger(0, COLORS.length - 1);
+  return COLORS[randomIndex];
 };
 
 const generateDate = () => {
